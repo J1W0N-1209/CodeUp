64a60093db0895c9e87fb2e74895168f7a8b4e36
jiwonKang <nabook754@naver.com>
2022-10-09 00:03:00 +0900

    CodeUp

---
+++ CodeUp39.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	long long num;
+	scanf("%lld",&num);
+	printf("%lld",num + 1);
+	return 0;
+}
---
+++ CodeUp41.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	int i,j,z;
+	scanf("%d %d %d",&i,&j,&z);
+	printf("%d\n",i + j + z);
+	printf("%.1f\n",(i + j + z) / 3.0);
+	return 0;
+}
