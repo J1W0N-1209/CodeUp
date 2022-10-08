0d64d95c9b483502860b55c81f8d455e76307260
jiwonKang <nabook754@naver.com>
2022-10-08 15:07:19 +0900

    CodeUp

---
+++ CodeUp11.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+	double d ;
+	scanf("%lf",&d);
+	printf("%lf",d);
+	return 0;
+}
---
+++ CodeUp17.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+	int year,month,day;
+	scanf("%d.%d.%d",&year,&month,&day);
+	printf("%04d.%02d.%02d",year,month,day);
+}
